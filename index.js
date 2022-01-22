1f7d50fed4aa90b88c4e0a7c1ccbb3be5b187a9d
YodaCode <76178582+YodaLightsabr@users.noreply.github.com>
2022-01-22 19:50:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const github = require('download-git-repo');
+const express = require('express');
+const fs = require('fs');
+
+function download () {
+    return new Promise((resolve, reject) => {
+        github('hackclub/dinosaurs', 'dinos', err => {
+            if (err) return reject(err);
+            const files = fs.readdirSync(__dirname + '/dinos').filter(file => file.endsWith('.png')).map(file => 'dinos/' + file);
+            fs.writeFileSync(__dirname + '/dinos.json', JSON.stringify(files, null, 4), 'utf8');
+            resolve(files);
+        });
+    });
+}
+
+function random (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+let dinos = [];
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Get a dino API. Use /dino or /dino.png to get a dino.');
+});
+
+app.get('/dino', (req, res) => {
+    res.sendFile(__dirname + '/' + dinos[random(0, dinos.length - 1)]);
+});
+
+app.get('/dino.png', (req, res) => {
+    res.sendFile(__dirname + '/' + dinos[random(0, dinos.length - 1)]);
+});
+
+download().then(downloaded => {
+    dinos = downloaded;
+    app.listen(3000, _ => {
+        console.log('Ready!');
+    });
+});
