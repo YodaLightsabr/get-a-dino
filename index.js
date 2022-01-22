824dfd921a00bc492ceda71bf5035bbe6679abca
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-01-22 21:21:18 +0000

    Change methods

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+const crypto = require('crypto');
 const github = require('download-git-repo');
 const express = require('express');
 const fs = require('fs');
+
+const home = `Welcome to Get-A-Dino! This website pulls dinosaurs from https://github.com/hackclub/dinosaurs.
+
+GET /dinos - Get the ID and URL to a random dino
+GET /dinos/:id - Serve a dino by it's id
+GET /info/:id - Get the info of a dino
+GET /random - Redirect to a random dino
+GET /dino.png - Serve a random dino as a raw PNG`;
 
 function download () {
     return new Promise((resolve, reject) => {
         github('hackclub/dinosaurs', 'dinos', err => {
             if (err) return reject(err);
+
             const files = fs.readdirSync(__dirname + '/dinos').filter(file => file.endsWith('.png')).map(file => 'dinos/' + file);
-            fs.writeFileSync(__dirname + '/dinos.json', JSON.stringify(files, null, 4), 'utf8');
-            resolve(files);
+
+            const dictionary = {};
+            files.forEach(file => {
+                dictionary[crypto.createHash('md5').update(file).digest('hex').substring(0, 10)] = file; // Really bad method of generating IDs but at least it is unlikely for duplicates and will be the same each time a dinos.json is generated
+            });
+
+            fs.writeFileSync(__dirname + '/dinos.json', JSON.stringify(dictionary, null, 4), 'utf8');
+
+            resolve(dictionary);
         });
     });
 }
@@ -21,15 +38,48 @@ let dinos = [];
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Get a dino API. Use /dino or /dino.png to get a dino.');
-});
-
-app.get('/dino', (req, res) => {
-    res.sendFile(__dirname + '/' + dinos[random(0, dinos.length - 1)]);
+    res.type('text/plain');
+    res.send(home);
 });
 
 app.get('/dino.png', (req, res) => {
-    res.sendFile(__dirname + '/' + dinos[random(0, dinos.length - 1)]);
+    res.sendFile(__dirname + '/' + Object.values(dinos)[random(0, Object.values(dinos).length - 1)]);
+});
+
+app.get('/dinos', (req, res) => {
+    const id = Object.keys(dinos)[random(0, Object.keys(dinos).length - 1)];
+    const name = dinos[id].substring(6, dinos[id].length - 4);
+    const url = 'https://geta.dino.icu/dinos/' + id;
+    const info = 'https://geta.dino.icu/info/' + id;
+    const github = 'https://github.com/hackclub/dinosaurs/blob/main/' + name + '.png';
+    
+    res.json({
+        id, name, url, info, github
+    });
+});
+
+app.get('/random', (req, res) => {
+    const id = Object.keys(dinos)[random(0, Object.keys(dinos).length - 1)];
+    const url = 'https://geta.dino.icu/dinos/' + id;
+    
+    res.redirect(url);
+});
+
+app.get('/dinos/:id', ({ params: { id } }, res) => {
+    if (!fs.existsSync(__dirname + '/' + dinos[id])) return res.json({ error: 'Dino not found' });
+    res.sendFile(__dirname + '/' + dinos[id]);
+});
+
+app.get('/info/:id', ({ params: { id } }, res) => {
+    if (!fs.existsSync(__dirname + '/' + dinos[id])) return res.json({ error: 'Dino not found' });
+    const name = dinos[id].substring(6, dinos[id].length - 4);
+    const url = 'https://geta.dino.icu/dinos/' + id;
+    const info = 'https://geta.dino.icu/info/' + id;
+    const github = 'https://github.com/hackclub/dinosaurs/blob/main/' + name + '.png';
+
+    res.json({
+        id, name, url, info, github
+    });
 });
 
 download().then(downloaded => {
